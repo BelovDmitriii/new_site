14085544bb9e0667ae40350c771f4cb2f63260ed
Dmitrii <belovd90@mail.ru>
2021-04-19 22:56:02 +0300

    films

--- js/script.js
+++ js/script.js
@@ -1,3 +1,22 @@
-"use strict";
-const user = "Alex";
-alert(`Привет, ${user}`);
+let numberOfFilms = prompt("Сколько фильмов Вы посмотрели?", "");
+
+
+
+
+let personalMovieDB = {
+    cont: numberOfFilms,
+    movies: {},
+    actors: {},
+    genres: [],
+    privat: false
+};
+
+let one = prompt("Один изпоследних просмотренных фильмов?", "");
+let two = prompt("На сколько оцените его?", "");
+let three = prompt("Один изпоследних просмотренных фильмов?", "");
+let four = prompt("На сколько оцените его?", "");
+
+personalMovieDB.movies[one] = two;
+personalMovieDB.movies[three] = four;
+
+console.log (personalMovieDB);
