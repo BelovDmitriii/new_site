affc6c4f8cfff96ef35673cac52152a6ef6b5fd3
Dmitrii <belovd90@mail.ru>
2021-04-20 00:30:54 +0300

    addCicles

--- js/script.js
+++ js/script.js
@@ -1,22 +1,40 @@
-let numberOfFilms = prompt("Сколько фильмов Вы посмотрели?", "");
+"use strict";
 
-
+let numberOfFilms = +prompt("Сколько фильмов Вы посмотрели?", "");
 
 
 let personalMovieDB = {
-    cont: numberOfFilms,
+    count: numberOfFilms,
     movies: {},
     actors: {},
     genres: [],
     privat: false
 };
 
-let one = prompt("Один изпоследних просмотренных фильмов?", "");
-let two = prompt("На сколько оцените его?", "");
-let three = prompt("Один изпоследних просмотренных фильмов?", "");
-let four = prompt("На сколько оцените его?", "");
 
-personalMovieDB.movies[one] = two;
-personalMovieDB.movies[three] = four;
+ for (let i = 0; i < 2; i++){
+     let a = prompt("Один из последних просмотренных фильмов?", "");
+     let b = prompt("На сколько оцените его?", "");
+
+     if(a != null && b != null && a != "" && b != "" && a.length <= 10){
+         personalMovieDB.movies[a] = b;
+     } else {
+         console.log('Error');
+         i--;
+     }
+ }
+
+
+let counter = personalMovieDB.count;
+
+if (counter < 10) {
+    console.log ("Просмотрено мало фильмов");
+} else if (counter >= 10 && counter < 30) {
+    console.log ("Вы классический зритель");
+} else if (counter >=30) {
+    console.log ("Вы киноман");
+} else {
+    console.log ("Ошибка!!!");
+}
 
 console.log (personalMovieDB);
